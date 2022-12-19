d267d92972801b28e0e7fe977b0eea7b7b013526
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-12-19 14:48:08 +0200

    Fixed forms layout jump for error state

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -123,7 +123,7 @@ export default function SignIn() {
                   onChange={(ev) => handleChange(ev, "username")}
                   value={userCredentials.username}
                   error={!!inputErrors.usernameError}
-                  helperText={inputErrors.usernameError}
+                  helperText={inputErrors.usernameError || " "}
                 />
               </Grid>
               <Grid
@@ -161,7 +161,7 @@ export default function SignIn() {
                     label="Password"
                   />
                   <FormHelperText error>
-                    {inputErrors.passwordError}
+                    {inputErrors.passwordError || " "}
                   </FormHelperText>
                 </FormControl>
               </Grid>
